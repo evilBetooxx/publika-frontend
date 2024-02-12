89efe226df620a1ee1ef948e30fe2be921244f8d
evilBetooxx <albertof4in@gmail.com>
2024-02-12 09:33:26 -0600

    ATLAS - Socket.io 'Implementado'

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,32 @@
 import LoadCard from "../card/LoadCard";
 import Layout from "../common/Layout";
+import { Toaster, toast } from "sonner";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
+import { usePost } from "../../context/PostContext";
+
+const socket = io("http://localhost:4000/publika");
 
 function Dashboard() {
+  const { notify } = usePost();
+
+  useEffect(() => {
+    socket.on("newPost", (title) => {
+      toast.success(`Nuevo anuncio publikado: ${title}`);
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    });
+
+    return () => {
+      socket.off("newPost");
+    };
+  }, [notify]);
+
   return (
     <Layout>
-      <LoadCard/>
+      <LoadCard />
+      <Toaster position='top-right' />
     </Layout>
   );
 }
